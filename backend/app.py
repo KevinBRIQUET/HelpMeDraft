cad84350a10bd1d8b1a2e9ec1016eec18d22fe03
KevinBRIQUET <kezin57@gmail.com>
2026-06-12 11:44:58 +0200

    Connecter le backend Flask à MySQL

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,41 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
 from flask import Flask
 
+load_dotenv()
 app = Flask(__name__)
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
 
 
 @app.get("/api/test")
 def test():
     return {"message": "Le backend fonctionne"}
 
+
+@app.get("/api/test-db")
+def test_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT DATABASE()")
+    database_name = cursor.fetchone()[0]
+
+    cursor.close()
+    connection.close()
+
+    return {
+        "message": "Connexion MySQL réussie",
+        "database": database_name
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
